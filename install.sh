a05c7a2da5697792cf89338f483117ffdebd24cc
Guilherme Pejon <guilpejon@gmail.com>
2019-11-04 23:39:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.gdf" ]; then
     echo "Installing GDF for the first time"
-    git clone --depth=1 https://github.com/guilpejon/gdf.git "$HOME/.gdf"
+    git clone --depth=1 https://github.com/guilpejon/dotfiles.git "$HOME/.gdf"
     cd "$HOME/.gdf"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
