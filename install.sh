b61236348d04b94814562376de6b05c938f0b823
Guilherme Pejon <guilpejon@gmail.com>
2019-11-24 23:09:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.gdf" ]; then
+  echo "Installing GDF for the first time"
+  git clone --depth=1 https://github.com/guilpejon/dotfiles.git "$HOME/.gdf"
+  # cp -R "$HOME/codes/gdf" "$HOME/.gdf"
+  cd "$HOME/.gdf"
+  [ "$1" = "ask" ] && export ASK="true"
+
   unameOut="$(uname -s)"
   case "${unameOut}" in
     Linux*)     machine=linux;;
@@ -19,12 +25,6 @@ if [ ! -d "$HOME/.gdf" ]; then
   else
     echo "Ruby already installed"
   fi
-
-  echo "Installing GDF for the first time"
-  git clone --depth=1 https://github.com/guilpejon/dotfiles.git "$HOME/.gdf"
-  # cp -R "$HOME/codes/gdf" "$HOME/.gdf"
-  cd "$HOME/.gdf"
-  [ "$1" = "ask" ] && export ASK="true"
 
   rake install
 else
